ff7f72daf2ddfb5db4eb6d751d2279b15b213abc
FachrielDzikri30 <112392440+FachrielDzikri30@users.noreply.github.com>
2023-04-28 14:13:53 +0700

    Update api.js

--- api.js
+++ api.js
@@ -10,27 +10,48 @@ fetch('https://api.covid19api.com/summary')
         const deathsAllCountries = document.getElementById("deathsAllCountries");
         const recoveredAllCountries = document.getElementById("recoveredAllCountries");
 
-        casesAllCountries.innerHTML = `Total Cases from All Countries : ${totalCasesAllCountries}`;
-        deathsAllCountries.innerHTML = `Total Deaths from All Countries : ${totalDeathsAllCountries}`;
-        recoveredAllCountries.innerHTML = `Total Recovered from All Countries : ${totalRecoveredAllCountries}`;
-        recoveredAllCountries.style.borderBottom = "5px solid black";
-        recoveredAllCountries.style.paddingBottom = "10px";
-    })
+        casesAllCountries.innerHTML = `Total Cases from All Countries : ${totalCasesAllCountries.toLocaleString()}`;
+        deathsAllCountries.innerHTML = `Total Deaths from All Countries : ${totalDeathsAllCountries.toLocaleString()}`;
+        recoveredAllCountries.innerHTML = `Total Recovered from All Countries : ${totalRecoveredAllCountries.toLocaleString()}`;
 
-    const clickHere= () => {
-        const inputIndex = document.getElementById("inputCountry").value;
-        console.log(inputIndex);
-        fetch('https://api.covid19api.com/summary')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.Countries[inputIndex].Country);
-            console.log(data.Countries[inputIndex].TotalConfirmed);
-            console.log(data.Countries[inputIndex].TotalDeaths);
-            console.log(data.Countries[inputIndex].TotalRecovered);
+        // for data summary by country
+        const inputCountry = document.getElementById("inputCountry");
+        const countryName = document.getElementById("countryName");
+        const casesCountry = document.getElementById("casesCountry");
+        const deathsCountry = document.getElementById("deathsCountry");
+        const recoveredCountry = document.getElementById("recoveredCountry");
+        const searchButton = document.getElementById("searchButton");
+        
 
-            document.getElementById("countryName").innerHTML = data.Countries[inputIndex].Country;
-            document.getElementById("casesCountry").innerHTML = "Total Cases : " +data.Countries[inputIndex].TotalConfirmed;
-            document.getElementById("deathsCountry").innerHTML = "Total Deaths : " +data.Countries[inputIndex].TotalDeaths;
-            document.getElementById("recoveredCountry").innerHTML = "Total Recovered : " +data.Countries[inputIndex].TotalRecovered;
+        let countryArray = [];
+
+        data.Countries.forEach(country => {
+            countryArray.push(country.Country)
         })
-    }
+        
+        searchButton.addEventListener('click',() => {
+            const inputCountry = document.getElementById("inputCountry").value;
+            if (inputCountry === '') {
+                alert('Search Box Can Not be Empty!');
+            }
+            else {
+                const result = data.Countries.filter(country => {
+                    return country.Country.toLowerCase().includes(inputCountry.toLowerCase());
+                });
+                if (result.length === 0) {
+                    countryName.innerHTML = "Country is Not Registered";
+                    casesCountry.innerHTML = "Total Confirmed : null";
+                    deathsCountry.innerHTML = "Total Deaths : null";
+                    recoveredCountry.innerHTML = "Total Recovered : null";
+                }
+                else {
+                    result.forEach(country => {
+                        countryName.innerHTML = `${country.Country}`;
+                        casesCountry.innerHTML = `Total Confirmed : ${country.TotalConfirmed.toLocaleString()}`;
+                        deathsCountry.innerHTML = `Total Deaths : ${country.TotalDeaths.toLocaleString()}`;
+                        recoveredCountry.innerHTML = `Total Recovered : ${country.TotalRecovered.toLocaleString()}`;                        
+                    })
+                }
+            }
+        })
+    })
